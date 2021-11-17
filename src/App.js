ec1672e4143aa3b507b986ab48a90a82a59d05be
llamar <thatlivelyllama@gmail.com>
2021-11-17 13:15:11 +0000

    Started weather frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      location:"",
+      clouds:"",
+      temperature:"",
+    }
+  }
+
+  componentDidMount(){
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=nottingham&units=imperial&appid=${process.env.REACT_APP_WEATHER_KEY}`
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>{
+      this.setState({
+        location: data.name,
+        clouds: data.weather[0].description,
+        temperature: data.main.temp,
+      })
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Weather</h1>
+        <h1>{this.state.location}</h1>
+        <h1>{this.state.clouds}</h1>
+        <h1>{this.state.temperature}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
